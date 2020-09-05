d9905af50a3503dc62465bcf7df4e19e004fcb97
Gary Thomas <gary@mlbassoc.com>
2020-09-05 15:28:06 +0200

    Initial simple building manager - WIP

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    [SerializeField] private Transform pfWoodHarvester;
+
+    private Camera _cam;
+
+    private void Awake() {
+        _cam = Camera.main;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition() {
+        Vector3 pos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0f;
+        return pos;
+
+    }
+}
