0ae333ffe4243748aa28da829ad2f288c5fc93bb
Gary Thomas <gary@mlbassoc.com>
2020-09-07 07:32:21 +0200

    Add camera handling - zoom, move - use Cinemachine

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraHandler : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private float minOrthographicSize = 8;
+    [SerializeField] private float maxOrthographicSize = 30;
+
+    private float orthographicSize;
+    private float targetOrthographicSize;
+
+    private void Start() {
+        orthographicSize = cam.m_Lens.OrthographicSize;
+        targetOrthographicSize = orthographicSize;
+    }
+
+    private void Update() {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleZoom() {
+        // Zoom in/out with mouse scroll wheel
+        targetOrthographicSize -= Input.mouseScrollDelta.y * zoomSpeed;
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+        cam.m_Lens.OrthographicSize = orthographicSize;
+    }
+
+    private void HandleMovement() {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 moveDir = new Vector3(x, y).normalized;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
