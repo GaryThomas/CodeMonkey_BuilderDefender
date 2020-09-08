4d8c73f15b45ab447cb013dadd4b360916de604e
Gary Thomas <gary@mlbassoc.com>
2020-09-08 08:05:27 +0200

    Add event when building type changes.  Refactor using Utils class

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BuildingManager : Singleton<BuildingManager> {
 
+    public event EventHandler<OnActiveBuildingTypeChangedEventArgs> OnActiveBuildingTypeChanged;
+
+    public class OnActiveBuildingTypeChangedEventArgs : EventArgs {
+        public BuildingTypeScriptableObject buildingType;
+    }
+
     private BuildingTypeScriptableObject _activeBuildingType;
     private BuildingTypeListScriptableObject _buildingTypes;
-    private Camera _cam;
 
     public override void Awake() {
         base.Awake();
@@ -19,23 +25,17 @@ public class BuildingManager : Singleton<BuildingManager> {
         _activeBuildingType = null;
     }
 
-    private void Start() {
-        _cam = Camera.main;
-    }
-
     private void Update() {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject() && _activeBuildingType) {
-            Instantiate(_activeBuildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(_activeBuildingType.prefab, Utils.GetMouseWorldPosition(), Quaternion.identity);
         }
-    }
-
-    private Vector3 GetMouseWorldPosition() {
-        Vector3 pos = _cam.ScreenToWorldPoint(Input.mousePosition);
-        pos.z = 0f;
-        return pos;
     }
 
     public void SelectBuildingType(BuildingTypeScriptableObject buildingType) {
         _activeBuildingType = buildingType;
+        Debug.Log("BM Select building " + buildingType.nameString + ", handler: " + OnActiveBuildingTypeChanged);
+        OnActiveBuildingTypeChanged?.Invoke(this,
+            new OnActiveBuildingTypeChangedEventArgs { buildingType = buildingType }
+        );
     }
 }
