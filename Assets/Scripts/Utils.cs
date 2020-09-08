7da203c3cf2d0bea047daa0db992d92abb3524cd
Gary Thomas <gary@mlbassoc.com>
2020-09-08 08:00:21 +0200

    Misc support functions

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils {
+
+    private static Camera _cam;
+
+    public static Vector3 GetMouseWorldPosition() {
+        if (_cam == null) {
+            _cam = Camera.main;
+        }
+        Vector3 pos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0f;
+        return pos;
+    }
+}
