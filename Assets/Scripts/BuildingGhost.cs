136c96edb495efbcad8095fed217afaefb138a79
Gary Thomas <gary@mlbassoc.com>
2020-09-08 08:07:48 +0200

    Show/track selected building via mouse

---
+++ Assets/Scripts/BuildingGhost.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGhost : MonoBehaviour {
+    [SerializeField] GameObject ghostSprite;
+
+    private void Awake() {
+        Hide();
+    }
+
+    private void Start() {
+        BuildingManager.Instance.OnActiveBuildingTypeChanged += ActiveBuildingTypeChanged;
+    }
+
+    private void ActiveBuildingTypeChanged(object sender, BuildingManager.OnActiveBuildingTypeChangedEventArgs e) {
+        // Debug.Log("Change building type to " + e.buildingType.nameString);
+        if (e.buildingType == null) {
+            Hide();
+        } else {
+            Show(e.buildingType.sprite);
+        }
+    }
+
+    private void Update() {
+        transform.position = Utils.GetMouseWorldPosition();
+    }
+
+    public void Show(Sprite sprite) {
+        ghostSprite.SetActive(true);
+        ghostSprite.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+    public void Hide() {
+        ghostSprite.SetActive(false);
+    }
+}
