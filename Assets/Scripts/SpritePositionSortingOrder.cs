83f05bfe65e61ed53a31e7e6c04ec09862310825
Gary Thomas <gary@mlbassoc.com>
2020-09-08 12:02:07 +0200

    Script to dynamically set sprite ordering - either dynamically or static

---
+++ Assets/Scripts/SpritePositionSortingOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritePositionSortingOrder : MonoBehaviour {
+    [SerializeField] private float precision = 5f;
+    [SerializeField] private bool fixedPos = false;
+
+    private SpriteRenderer _sr;
+
+    private void Awake() {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Select sprite order in layer based on Y position
+    private void LateUpdate() {
+        _sr.sortingOrder = (int)(-transform.position.y * precision);
+        if (fixedPos) {
+            // This sprite/object will never move so disable this script [SCARY]
+            Destroy(this);
+        }
+    }
+}
