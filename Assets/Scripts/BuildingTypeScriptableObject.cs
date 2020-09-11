2864b9d1cdb473065858ac78611c13fb15d1da5e
Gary Thomas <gary@mlbassoc.com>
2020-09-11 09:56:29 +0200

    Add production costs and a way to get them for display

--- Assets/Scripts/BuildingTypeScriptableObject.cs
+++ Assets/Scripts/BuildingTypeScriptableObject.cs
@@ -10,4 +10,14 @@ public class BuildingTypeScriptableObject : ScriptableObject {
     public ResourceGeneratorData resourceGeneratorData;
     public float minSeparation;
     public ResourceAmount[] productionCosts;
+
+    public string GetProductionCostsString() {
+        string str = "";
+        bool first = true;
+        foreach (ResourceAmount cost in productionCosts) {
+            str += (first ? "" : ", ") + cost.resource.nameString + ": " + cost.amount;
+            first = false;
+        }
+        return str;
+    }
 }
