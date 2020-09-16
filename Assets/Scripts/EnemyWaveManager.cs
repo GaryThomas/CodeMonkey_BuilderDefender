706f9f8ba1889367ed37db41b2e28bb6ab050da0
Gary Thomas <gary@mlbassoc.com>
2020-09-16 09:13:36 +0200

    Add enemy wave generation (automatic) + UI to give info about it

---
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveManager : MonoBehaviour {
+
+    public event EventHandler OnWaveNumberChanged;
+
+    [SerializeField] private float timeToNextWave;
+    [SerializeField] private float timeToSpawnEnemy;
+    [SerializeField] private int startingWaveSize = 5;
+    [SerializeField] private int waveMultiplier = 3;
+    [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private Transform nextSpawnPoint;
+
+    private enum State {
+        WaitingToSpawnWave,
+        SpawningWave,
+    }
+    private State _state;
+    private float _waveSpawnTimer;
+    private float _enemySpawnTimer;
+    private int _enemiesToSpawn;
+    private Vector3 _spawnPoint;
+    private int _waveNumber;
+
+    private void Start() {
+        _state = State.WaitingToSpawnWave;
+        _waveSpawnTimer = 5f;
+        NextSpawnPosition();
+    }
+
+    private void Update() {
+        switch (_state) {
+            case State.WaitingToSpawnWave:
+                _waveSpawnTimer -= Time.deltaTime;
+                if (_waveSpawnTimer < 0) {
+                    SpawnWave();
+                }
+                break;
+            case State.SpawningWave:
+                if (_enemiesToSpawn > 0) {
+                    _enemySpawnTimer -= Time.deltaTime;
+                    if (_enemySpawnTimer < 0) {
+                        _enemySpawnTimer = UnityEngine.Random.Range(0, timeToSpawnEnemy);
+                        _enemiesToSpawn--;
+                        Enemy.CreateEnemy(_spawnPoint + Utils.GetRandomDir() * UnityEngine.Random.Range(0, 10f));
+                    }
+                } else {
+                    _waveSpawnTimer = timeToNextWave;
+                    _state = State.WaitingToSpawnWave;
+                    NextSpawnPosition();
+                }
+                break;
+        }
+    }
+
+    private void SpawnWave() {
+        _enemiesToSpawn = startingWaveSize + _waveNumber * waveMultiplier;
+        _enemySpawnTimer = timeToSpawnEnemy;
+        _state = State.SpawningWave;
+        _waveNumber++;
+        OnWaveNumberChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void NextSpawnPosition() {
+        _spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Count)].position;
+        nextSpawnPoint.position = _spawnPoint;
+    }
+
+    public int GetWaveNumber() {
+        return _waveNumber;
+    }
+
+    public float GetTimeToNextWave() {
+        return _waveSpawnTimer;
+    }
+}
---
+++ Assets/Scripts/EnemyWaveUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class EnemyWaveUI : MonoBehaviour {
+    [SerializeField] EnemyWaveManager enemyWaveManager;
+    [SerializeField] private TextMeshProUGUI waveNumberText;
+    [SerializeField] private TextMeshProUGUI waveTimerText;
+
+    private void Start() {
+        enemyWaveManager.OnWaveNumberChanged += WaveNumberChanged;
+    }
+
+    private void SetWaveNumberText(string str) {
+        waveNumberText.SetText(str);
+    }
+
+    private void SetWaveTimerText(string str) {
+        waveTimerText.SetText(str);
+    }
+
+    private void WaveNumberChanged(object sender, EventArgs e) {
+        SetWaveNumberText("Wave #" + (enemyWaveManager.GetWaveNumber() + 1));
+    }
+
+    private void Update() {
+        float timeToNextWave = enemyWaveManager.GetTimeToNextWave();
+        if (timeToNextWave < 0f) {
+            SetWaveTimerText("");
+        } else {
+            SetWaveTimerText("Next wave in " + timeToNextWave.ToString("F1") + " secs");
+        }
+    }
+}
