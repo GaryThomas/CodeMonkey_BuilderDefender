346271b43fcc212cf45e228c49743e0d7577961d
Gary Thomas <gary@mlbassoc.com>
2020-09-20 09:22:48 +0200

    Add a button on each building/factory to demolish it and return part of the resources used to build it

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Building : MonoBehaviour {
+    [SerializeField] Transform buildingDemolishButton;
+
     private HealthSystem _health;
     private BuildingTypeScriptableObject _buildingType;
 
@@ -12,10 +14,31 @@ public class Building : MonoBehaviour {
         _health = GetComponent<HealthSystem>();
         _health.OnDeath += OnDeath;
         _health.SetMaxHealth(_buildingType.maxHealth);
+        HideDemolishButton();
     }
 
     private void OnDeath(object sender, EventArgs e) {
         Debug.Log(_buildingType.nameString + " Died!");
         Destroy(gameObject);
+    }
+
+    private void OnMouseEnter() {
+        ShowDemolishButton();
+    }
+
+    private void OnMouseExit() {
+        HideDemolishButton();
+    }
+
+    private void ShowDemolishButton() {
+        if (buildingDemolishButton != null) {
+            buildingDemolishButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void HideDemolishButton() {
+        if (buildingDemolishButton != null) {
+            buildingDemolishButton.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/BuildingDemolishButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingDemolishButton : MonoBehaviour {
+    [SerializeField] Button button;
+    [SerializeField] Building building;
+
+    private void Awake() {
+        button.onClick.AddListener(() => {
+            BuildingTypeScriptableObject buildingType;
+            buildingType = building.GetComponent<BuildingTypeRef>().buildingType;
+            foreach (ResourceAmount resourceAmount in buildingType.productionCosts) {
+                ResourceManager.Instance.AddResources(resourceAmount.resource, Mathf.FloorToInt(resourceAmount.amount * 0.60f));
+            }
+            Destroy(building.gameObject);
+        });
+    }
+}
