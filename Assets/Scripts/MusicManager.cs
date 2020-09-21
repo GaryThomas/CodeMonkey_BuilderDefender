8b0a9fb1bf20a1afdfd8b7657c8d0f71c7692f9b
Gary Thomas <gary@mlbassoc.com>
2020-09-21 17:26:57 +0200

    Add options menu for sound/music settings

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : Singleton<SoundManager> {
-    public SoundClips clips;
-
+public class MusicManager : Singleton<MusicManager> {
     private AudioSource _audio;
     private float _volume = 0.5f;
 
     public override void Awake() {
         base.Awake();
         _audio = GetComponent<AudioSource>();
-    }
-
-    public void PlayClip(AudioClip clip) {
-        _audio.Stop();
-        _audio.PlayOneShot(clip, _volume);
+        UpdateVolume();
     }
 
     public void IncreaseVolume() {
         _volume = Mathf.Clamp01(_volume + 0.1f);
+        UpdateVolume();
     }
 
     public void DecreaseVolume() {
         _volume = Mathf.Clamp01(_volume - 0.1f);
+        UpdateVolume();
     }
 
     public float GetVolume() {
         return _volume;
+    }
+
+    private void UpdateVolume() {
+        _audio.volume = _volume;
     }
 }
---
+++ Assets/Scripts/OptionsUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsUI : MonoBehaviour {
+    [SerializeField] private Button soundIncreaseButton;
+    [SerializeField] private Button soundDecreaseButton;
+    [SerializeField] private TextMeshProUGUI soundVolumeText;
+    [SerializeField] private Button musicIncreaseButton;
+    [SerializeField] private Button musicDecreaseButton;
+    [SerializeField] private TextMeshProUGUI musicVolumeText;
+    [SerializeField] private Button mainMenuButton;
+
+    private void Awake() {
+        soundIncreaseButton.onClick.AddListener(() => {
+            SoundManager.Instance.IncreaseVolume();
+            ShowSoundVolume();
+        });
+        soundDecreaseButton.onClick.AddListener(() => {
+            SoundManager.Instance.DecreaseVolume();
+            ShowSoundVolume();
+        });
+        musicIncreaseButton.onClick.AddListener(() => {
+            MusicManager.Instance.IncreaseVolume();
+            ShowMusicVolume();
+        });
+        musicDecreaseButton.onClick.AddListener(() => {
+            MusicManager.Instance.DecreaseVolume();
+            ShowMusicVolume();
+        });
+    }
+
+    private void Start() {
+        ShowMusicVolume();
+        ShowSoundVolume();
+        gameObject.SetActive(false);
+    }
+
+    private void ShowSoundVolume() {
+        soundVolumeText.SetText(Mathf.RoundToInt(SoundManager.Instance.GetVolume() * 10f).ToString());
+    }
+
+    private void ShowMusicVolume() {
+        musicVolumeText.SetText(Mathf.RoundToInt(MusicManager.Instance.GetVolume() * 10f).ToString());
+    }
+
+    public void ToggleOptionsUI() {
+        gameObject.SetActive(!gameObject.activeSelf);
+        if (gameObject.activeSelf) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+}
